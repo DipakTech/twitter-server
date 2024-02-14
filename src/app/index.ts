6c50b6d0f79282dd8717a45048f0a12ba3c2f4e3
Dipak Giri <dipakgiri1200@gmail.com>
2024-02-14 20:16:51 +0545

    user types query and mutation

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,33 +3,25 @@ import bodyParser from 'body-parser'
 import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
 import { prismaClient } from '../clients/db'
+import { User } from './user'
 
 export async function initServer() {
   const app = express()
 
   app.use(bodyParser.json())
 
-  await prismaClient.user.create({
-    data: {
-      email: 'dipakgiri.dev@gmail.com',
-      firstName: 'dipak',
-      lastName: 'giri',
-    },
-  })
-
   const graphqlServer = new ApolloServer({
     typeDefs: `
+    ${User.types}
+
     type Query{
-      sayHello:String
-      sayHelloToMe(name:String!):String!
+      ${User.queries}
     }
 
     `,
     resolvers: {
       Query: {
-        sayHello: () => 'Hello from Twitter Server',
-        sayHelloToMe: (parent: any, { name }: { name: string }) =>
-          `Hello ${name}`,
+        ...User.resolvers.queries,
       },
     },
   })
